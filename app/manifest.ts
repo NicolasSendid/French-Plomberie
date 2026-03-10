c710092b50e271b5f17a7a20de96d8b3d12b01ea
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-10 14:12:20 +0100

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,1 +1,26 @@
+import { MetadataRoute } from 'next'
 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'French Plomberie',
+    short_name: 'Fr Plomberie',
+    description: 'Dépannage et installation Plomberie, Chauffage, Salle de Bains et Cuisine',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#2563eb', // Le bleu de ton header
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
