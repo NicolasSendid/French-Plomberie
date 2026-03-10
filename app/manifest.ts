0176755a5e27354ef739cbfa54e799b4f6a29874
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-10 17:04:28 +0100

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#2563eb', // Le bleu de ton header
     icons: [
       {
-        src: '/logo.png',
+        src: '/public/logo.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/logo.png',
+        src: '/public/logo.png',
         sizes: '512x512',
         type: 'image/png',
       },
