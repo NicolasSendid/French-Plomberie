788bf448ab794ee5e963485cc4c92f20ed75abbe
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-12 10:08:46 +0100

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
+
   try {
+
     const formData = await req.formData();
 
     const prenom = formData.get("prenom");
@@ -12,35 +14,75 @@ export async function POST(req: Request) {
     const prestation = formData.get("prestation");
     const message = formData.get("message");
 
+    const photos = formData.getAll("photos");
+
     const transporter = nodemailer.createTransport({
+
       host: process.env.SMTP_HOST,
       port: 587,
       secure: false,
+
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
+        pass: process.env.SMTP_PASS
+      }
+
     });
 
+    const attachments = await Promise.all(
+
+      photos.map(async (photo:any) => {
+
+        if (!photo || photo.size === 0) return null;
+
+        const buffer = Buffer.from(await photo.arrayBuffer());
+
+        return {
+          filename: photo.name,
+          content: buffer
+        };
+
+      })
+
+    );
+
+    const filtered = attachments.filter(Boolean);
+
     await transporter.sendMail({
+
       from: `"French Plomberie" <${process.env.SMTP_USER}>`,
+
       to: "frenchplomberie@gmail.com",
-      subject: "Nouvelle demande plomberie",
+
+      subject: "🚰 Nouvelle demande plomberie",
+
       text: `
 Nom : ${prenom} ${nom}
 Téléphone : ${tel}
 Email : ${email}
+
 Adresse : ${adresse}
+
 Prestation : ${prestation}
 
 Message :
 ${message}
       `,
+
+      attachments: filtered
+
     });
 
     return Response.json({ success: true });
+
   } catch (error) {
-    console.error(error);
-    return Response.json({ success: false });
+
+    console.error("Erreur API :", error);
+
+    return Response.json(
+      { success: false },
+      { status: 500 }
+    );
+
   }
 }
